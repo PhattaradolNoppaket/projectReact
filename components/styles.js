a15f6e47b918b97789f04af83277e83ab1a25b83
Phattaradol Noppaket <Copterhyperdragon@gmail.com>
2022-06-22 09:20:01 +0700

    first commit

---
+++ components/styles.js
@@ -0,0 +1,21 @@
+import { StyleSheet, Text, View } from 'react-native'
+
+
+
+
+const styles = StyleSheet.create({
+    container:{
+      flex:1,
+      alignItems:'center',
+      justifyContent:'center'
+    },
+  
+  title:{
+  fontSize: 20,
+    },
+  warning:{
+    color: 'Red'
+  }
+  
+  });
+  export default styles
